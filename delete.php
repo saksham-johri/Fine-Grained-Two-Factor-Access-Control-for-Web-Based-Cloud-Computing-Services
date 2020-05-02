1be3a7be0540af294096fb67ff20d0f85b5dc822
Harshit <hcool2489@gmail.com>
2020-05-03 04:36:51 +0530

    Delete Module

---
+++ delete.php
@@ -0,0 +1,66 @@
+<?php
+	require 'vendor/autoload.php';
+	
+	use Aws\S3\S3Client;
+	use Aws\S3\Exception\S3Exception;
+
+	include('aws-config.php');
+	
+	// Connect to AWS
+	try {
+		$s3 = S3Client::factory(
+			array(
+				'credentials' => array(
+					'key' => $IAM_KEY,
+					'secret' => $IAM_SECRET
+				),
+				'version' => 'latest',
+				'region'  => $REGION
+			)
+		);
+	} catch (Exception $e) {
+		header('Location: ' . $RedirectURL . '?result=failed&f=' . $folder);
+		die("Error: " . $e->getMessage());
+	}
+
+
+	$keyname = $_POST['keyName'];
+
+	// 1. Delete the object from the bucket.
+	try
+	{
+		echo 'Attempting to delete ' . $keyname . '...' . PHP_EOL;
+
+		$result = $s3->deleteObject([
+			'Bucket' => $bucketName,
+			'Key'    => $keyname
+		]);
+
+		if ($result['DeleteMarker'])
+		{
+			echo $keyname . ' was deleted or does not exist.' . PHP_EOL;
+		} else {
+			exit('Error: ' . $keyname . ' was not deleted.' . PHP_EOL);
+		}
+	}
+	catch (S3Exception $e) {
+		exit('Error: ' . $e->getAwsErrorMessage() . PHP_EOL);
+	}
+
+	// 2. Check to see if the object was deleted.
+	try
+	{
+		echo 'Checking to see if ' . $keyname . ' still exists...' . PHP_EOL;
+
+		$result = $s3->getObject([
+			'Bucket' => $bucketName,
+			'Key'    => $keyname
+		]);
+
+		echo 'Error: ' . $keyname . ' still exists.';
+	}
+	catch (S3Exception $e) {
+		exit($e->getAwsErrorMessage());
+	}
+	header('Location: ' . $RedirectURL . '?result=success');
+?>
