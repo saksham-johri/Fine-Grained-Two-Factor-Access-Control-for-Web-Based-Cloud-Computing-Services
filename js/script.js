420e09ca0223cc12dd6633de10d8aa3e7646c26e
saksham8979 <sakshamjohri@gmail.com>
2020-05-03 12:59:29 +0530

    login page changed

--- js/script.js
+++ js/script.js
@@ -1,28 +1,62 @@
-$('#password').focusin(function(){
-  $('form').addClass('up')
-});
-$('#password').focusout(function(){
-  $('form').removeClass('up')
-});
-
-// Panda Eye move
-$(document).on( "mousemove", function( event ) {
-  var dw = $(document).width() / 15;
-  var dh = $(document).height() / 15;
-  var x = event.pageX/ dw;
-  var y = event.pageY/ dh;
-  $('.eye-ball').css({
-    width : x,
-    height : y
+$(document).ready(function() {
+  
+  var animating = false,
+      submitPhase1 = 1100,
+      submitPhase2 = 400,
+      logoutPhase1 = 800,
+      $login = $(".login"),
+      $app = $(".app");
+  
+  function ripple(elem, e) {
+    $(".ripple").remove();
+    var elTop = elem.offset().top,
+        elLeft = elem.offset().left,
+        x = e.pageX - elLeft,
+        y = e.pageY - elTop;
+    var $ripple = $("<div class='ripple'></div>");
+    $ripple.css({top: y, left: x});
+    elem.append($ripple);
+  };
+  
+  $(document).on("click", ".login__submit", function(e) {
+    if (animating) return;
+    animating = true;
+    var that = this;
+    ripple($(that), e);
+    $(that).addClass("processing");
+    setTimeout(function() {
+      $(that).addClass("success");
+      setTimeout(function() {
+        $app.show();
+        $app.css("top");
+        $app.addClass("active");
+      }, submitPhase2 - 70);
+      setTimeout(function() {
+        $login.hide();
+        $login.addClass("inactive");
+        animating = false;
+        $(that).removeClass("success processing");
+      }, submitPhase2);
+    }, submitPhase1);
   });
-});
-
-// validation
-
-
-$('.btn').click(function(){
-  $('form').addClass('wrong-entry');
-    setTimeout(function(){ 
-       $('form').removeClass('wrong-entry');
-     },3000 );
+  
+  $(document).on("click", ".app__logout", function(e) {
+    if (animating) return;
+    $(".ripple").remove();
+    animating = true;
+    var that = this;
+    $(that).addClass("clicked");
+    setTimeout(function() {
+      $app.removeClass("active");
+      $login.show();
+      $login.css("top");
+      $login.removeClass("inactive");
+    }, logoutPhase1 - 120);
+    setTimeout(function() {
+      $app.hide();
+      animating = false;
+      $(that).removeClass("clicked");
+    }, logoutPhase1);
+  });
+  
 });
