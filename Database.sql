fddef9347ec1ff7636c806d9afa24e29b4c9c264
Harshit <hcool2489@gmail.com>
2020-05-04 11:54:00 +0530

    Database Update

--- Database.sql
+++ Database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 03, 2020 at 06:16 AM
+-- Generation Time: May 04, 2020 at 06:18 AM
 -- Server version: 5.7.26
 -- PHP Version: 5.6.40
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Evolve`
+-- Database: `evolves`
 --
 
 -- --------------------------------------------------------
@@ -31,14 +31,16 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `tbl_users`;
 CREATE TABLE IF NOT EXISTS `tbl_users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(120) DEFAULT NULL,
-  `password` varchar(200) DEFAULT NULL,
+  `email` varchar(120) NOT NULL,
+  `password` varchar(200) NOT NULL,
   `password_last_change` timestamp NOT NULL,
   `profile_name` varchar(100) DEFAULT NULL,
   `google_auth_code` varchar(16) DEFAULT NULL,
-  `gauth_qr_last_scan` timestamp NOT NULL,
-  `last_login` timestamp NOT NULL,
-  PRIMARY KEY (`user_id`)
+  `gauth_qr_last_scan` timestamp NULL DEFAULT NULL,
+  `last_login` timestamp NULL DEFAULT NULL,
+  `user_type` varchar(10) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
